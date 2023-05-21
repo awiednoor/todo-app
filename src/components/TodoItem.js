792161a6e5f361f231d49200f45f5b1778bc4824
Noor Awied <nooorawied@gmail.com>
2023-05-21 23:31:33 +0300

    :zap: added task deletion confirmation

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { toggleTodo, deleteTodo } from '../state/todoReducer';
 
 function TodoItem({ todo }) {
+  const [alertMessage, setAlertMessage] = useState({
+    show: false,
+    title: '',
+  });
   const dispatch = useDispatch();
-  const handleDelete = (task) => {
-    alert(`Are you sure you want to delete :\n ${task.title}`);
+  const handleOk = (task) => {
+    setAlertMessage({
+      show: true,
+      title: task.title,
+    });
     dispatch(deleteTodo(task.id));
+    setAlertMessage({
+      show: false,
+      title: '',
+    });
+  };
+  const handleCancel = () => {
+    setAlertMessage({
+      show: false,
+      title: '',
+    });
   };
   return (
     <div className="w-full flex-col">
@@ -30,12 +47,39 @@ function TodoItem({ todo }) {
 
         <button
           className=" w-20 h-10 bg-neutral-600 rounded-full font-bold hover:bg-rose-700 "
-          onClick={() => handleDelete(todo)}
+          onClick={() =>
+            setAlertMessage({
+              show: true,
+              title: todo.title,
+            })
+          }
         >
           Delete
         </button>
       </div>
       <p className="my-4 px-10">{todo.description}</p>
+      {alertMessage.show && (
+        <div className="w-full h-full fixed top-0 left-0 flex justify-center items-center bg-gray-900 bg-opacity-50">
+          <div className="flex flex-col bg-neutral-800 border-2 rounded-xl border-neutral-600 w-1/3 h-1/3 flex justify-center items-center relative">
+            <p>Are you sure you want to delete:</p>
+            <p>{alertMessage.title}</p>
+            <div className="w-1/3 flex justify-between items-center mt-8 ">
+              <button
+                className=" w-20 h-10 bg-rose-600 rounded-full font-bold hover:bg-rose-700 "
+                onClick={() => handleOk(todo)}
+              >
+                Yes
+              </button>
+              <button
+                className=" w-20 h-10 bg-neutral-500 hover:bg-neutral-600 rounded-full font-bold  "
+                onClick={handleCancel}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
