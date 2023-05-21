7265fefbf0620faeaa41e6c19bab0458c2f3af9d
Noor Awied <nooorawied@gmail.com>
2023-05-21 15:44:55 +0300

    :white_check_mark: added redux toolkit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { todoStore } from './state/todoStore';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={todoStore}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
