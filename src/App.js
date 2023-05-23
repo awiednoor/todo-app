fb25833208f9fa7aabb9c0ad54341de462c725a9
Noor Awied <nooorawied@gmail.com>
2023-05-23 20:44:39 +0300

    :lipstick: styling changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux';
 function App() {
   const todoStates = useSelector((state) => state.todos.todoStates);
   return (
-    <div className="w-full min-h-screen max-h-content bg-neutral-800 text-white pt-32">
+    <div className="w-full min-h-screen bg-neutral-800 text-white pt-32">
       <TodoModal />
       {todoStates.map((state) => (
         <TodoList type={state} />
